56e0419056bc15d5283bb11b55ec4722817f1959
vinicius.pita <vinicius.pita@wirecard.com>
2020-10-08 21:57:34 -0300

    add: adding

---
+++ src/test/java/br/com/viniciuspenha/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspenha.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
